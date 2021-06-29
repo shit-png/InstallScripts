73039547bab7c984043a1c6d546db7b061b6e16a
shit-png <52354974+shit-png@users.noreply.github.com>
2021-06-29 13:17:56 +0200

    Edit teams-for-linux.sh

--- teams-for-linux.sh
+++ teams-for-linux.sh
@@ -2,4 +2,12 @@ sudo apt update
 sudo apt install snapd
 sudo snap install core
 sudo snap install teams-for-linux
-echo "/snap/teams-for-linux/current/teams-for-linux" > ~/Desktop/execute-Teams.sh
+echo "[Desktop Entry]
+Name=Teams
+Comment=Runs Teams
+Icon=/home/pi/Downloads/Teams.png
+Exec=/snap/teams-for-linux/current/teams-for-linux
+Type=Application
+Encoding=UTF-8
+Terminal=false
+Categories=None;" > /home/pi/Desktop/Teams.Desktop
