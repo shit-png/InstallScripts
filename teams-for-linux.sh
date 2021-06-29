c6ef85ad19551c37afeeb59067a56b34e16bc74a
shit-png <52354974+shit-png@users.noreply.github.com>
2021-06-29 13:02:14 +0200

    Edited Teams install script

--- teams-for-linux.sh
+++ teams-for-linux.sh
@@ -1,6 +1,5 @@
 sudo apt update
 sudo apt install snapd
-sudo reboot
 sudo snap install core
 sudo snap install teams-for-linux
 echo "/snap/teams-for-linux/current/teams-for-linux" > ~/Desktop/execute-Teams.sh
